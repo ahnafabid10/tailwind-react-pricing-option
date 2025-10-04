954c334d922e1dda62ed16056203e28146a1baef
Ahnaf Abid <ahnafabid600@gmail.com>
2025-10-04 18:09:48 +0600

    first commit

--- src/PricingCard.jsx
+++ src/PricingCard.jsx
@@ -5,19 +5,20 @@ const PricingCard = ({pricing}) => {
     
     console.log(pricing)
     return (
-        <div className='border bg-[#d3d3d3] rounded-2xl p-4'>
+        <div className='flex flex-col border bg-[#d3d3d3] rounded-2xl p-4'>
             {/* card header */}
             <div>
             <h1 className='text-7xl'>{pricing.name}</h1>
             <h4 className='text-3xl'>${pricing.price}</h4>
             </div>
             {/* card Body */}
-            <div>
-            <p className='bg-amber-600 p-4 rounded-2xl mt-10'>{pricing.description}</p>
+            <div className='bg-amber-600 p-4 rounded-2xl mt-10 flex-1'>
+            <p >{pricing.description}</p>
             {
                 pricing.features.map((feature, index) => <PricingFeatures key={index} feature ={feature}></PricingFeatures>)
             }
             </div>
+            <button className="btn w-full">Subscribe</button>
         </div>
     );
 };
